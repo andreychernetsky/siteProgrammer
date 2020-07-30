38d91e6b89c873215e529d4b6fe826f5f48295bb
andric <rodomir3119841985@gmail.com>
2020-07-30 17:32:20 +0300

    vidos

--- gulpfile.js
+++ gulpfile.js
@@ -14,9 +14,9 @@ gulp.task('clean', async function(){
 
 gulp.task('scss', function(){
   return gulp.src('app/scss/**/*.scss')
-    .pipe(sass({outputStyle: 'expanded'})) //expanded compressed
+    .pipe(sass({outputStyle: 'compressed'}))
     .pipe(autoprefixer({
-      overrideBrowserslist: ['last 8 versions']
+      browsers: ['last 8 versions']
     }))
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('app/css'))
